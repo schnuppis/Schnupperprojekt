dd0668e07ac44ba974b0500450e7da6f29067927
schnuppis <schnuppis.bit@gmail.com>
2023-05-09 14:43:58 +0200

    initial commit

---
+++ Schnupperspiel/Münzenjäger.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Windows.Forms;
+
+// !! ALS STARTDATEI "Schnupperspiel.csproj - Debug|AnyCPU" auswählen !!
+
+namespace Schnupperspiel
+{
+    public partial class frmGame : Form
+    {
+        public Game game = new Game();
+        private Random random = new Random();
+        public static int xPlayer = 750;
+        public static int yPlayer = 450;
+
+        public Panel gamePanel;
+
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+        private void loadGame(object sender, EventArgs e)
+        {
+            
+            //game.setFormColour(200, 200, 200);
+
+            //gamePanel = new Panel();
+            //gamePanel.setSize();
+            //gamePanel.setColour();
+           
+
+            
+
+            game.makeGame(this);
+        }
+        private void movePlayer(object sender, KeyEventArgs key)
+        {
+            /*Player player = gamePanel.getPlayer();
+            if (key.KeyCode == Keys.D && game.checkPanelRight())
+            {
+                
+            }
+            if (key.KeyCode == Keys.A && game.checkPanelLeft())
+            {
+                
+            }
+            if (key.KeyCode == Keys.W && game.checkPanelTop())
+            {
+                
+            }
+            if (key.KeyCode == Keys.S && game.checkPanelBottom())
+            {
+                
+            }*/
+        }
+        /*private void tmrGame_Tick(object sender, EventArgs e)
+        {
+        }*/
+    }
+}
