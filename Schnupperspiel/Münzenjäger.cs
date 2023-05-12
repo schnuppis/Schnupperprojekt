a7a1db12ce9e0590f44641f44d554f8c13a91260
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 11:09:08 +0200

    Aufgabe 3

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -1,7 +1,9 @@
+using Schnupperspiel;
 using System;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
-// !! ALS STARTDATEI "Schnupperspiel.csproj - Debug|AnyCPU" auswählen !!
+ !! ALS STARTDATEI "Schnupperspiel.csproj - Debug|AnyCPU" auswählen !!
 
 namespace Schnupperspiel
 {
@@ -21,21 +23,49 @@ namespace Schnupperspiel
         private void loadGame(object sender, EventArgs e)
         {
             
-            //game.setFormColour(200, 200, 200);
+            game.setFormColour(200, 200, 200);
 
-            //gamePanel = new Panel();
-            //gamePanel.setSize();
-            //gamePanel.setColour();
+            gamePanel = new Panel();
+            gamePanel.setSize(800,500);
+            gamePanel.setColour(0,0,0);
            
+            game.setPanel(gamePanel); 
 
-            
+            Label labelName = new Label();
+            labelName.setPosition(820, 480);
+            labelName.setSize(250, 55);
+            labelName.setText("CoinHunter:");
+            game.add(labelName);
+           
+            Label labeltime = new Label();
+            labeltime.setPosition(820, 10);
+            labeltime.setSize(220, 55);
+            labeltime.setText("Text:");
+           
+            Label labelpoints = new Label();
+            labelpoints.setPosition(820, 70);
+            labelpoints.setSize(220, 55);
+            labelpoints.setText("Points:");
+            game.add(labelName);
+           
+            Label labelhighscore = new Label();
+            labelhighscore.setPosition(820, 130);
+            labelhighscore.setSize(220, 55);
+            labelhighscore.setText("Highscore:");
+            game.add(labelName);
+
+            Text textName = new Text();
+            addTimeText(textName)
+            addPointsText(textName)
+            addHighscoreText(textName)
+
 
             game.makeGame(this);
         }
         private void movePlayer(object sender, KeyEventArgs key)
         {
             /*Player player = gamePanel.getPlayer();
-            if (key.KeyCode == Keys.D && game.checkPanelRight())
+            /if (key.KeyCode == Keys.D && game.checkPanelRight())
             {
                 
             }
