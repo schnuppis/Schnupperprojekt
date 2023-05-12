22a60f77afe7f67884b6cf66a81576d3174af8c4
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 12:45:35 +0200

    (Aufgabe5)

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing.Text;
 using System.Windows.Forms;
 
 // !! ALS STARTDATEI "Schnupperspiel.csproj - Debug|AnyCPU" auswählen !!
@@ -20,12 +21,12 @@ namespace Schnupperspiel
         }
         private void loadGame(object sender, EventArgs e)
         {
-            
+
             game.setFormColour(200, 200, 200);
 
             gamePanel = new Panel();
-            gamePanel.setSize(800,500);
-            gamePanel.setColour(0,0,0);
+            gamePanel.setSize(800, 500);
+            gamePanel.setColour(0, 0, 0);
             game.setPanel(gamePanel);
 
             Label labelName = new Label();
@@ -36,63 +37,93 @@ namespace Schnupperspiel
             game.add(labelName);
 
             Label labelName2 = new Label();
-            labelName2.setPosition(820,10);
-            labelName2.setSize(220,55);
+            labelName2.setPosition(820, 10);
+            labelName2.setSize(220, 55);
             labelName2.setText("Time:");
             game.add(labelName2);
 
             Label labelName3 = new Label();
             labelName3.setPosition(820, 70);
-            labelName3.setSize(220,55);
+            labelName3.setSize(220, 55);
             labelName3.setText("Points:");
             game.add(labelName3);
 
             Label labelName4 = new Label();
-            labelName4.setPosition(820,130);
-            labelName4.setSize(220,55);
+            labelName4.setPosition(820, 130);
+            labelName4.setSize(220, 55);
             labelName4.setText("Highscore");
             game.add(labelName4);
-            
+
             Text textName1 = new Text();
             textName1.setPosition(1050, 10);
             textName1.setSize(94, 44);
             game.addTimeText(textName1);
 
             Text textName2 = new Text();
-            textName2.setPosition(1050,70);
+            textName2.setPosition(1050, 70);
             textName2.setSize(94, 44);
-            game.addHighscoreText(textName2);
+            game.addPointsText(textName2);
 
             Text textName3 = new Text();
             textName3.setPosition(1050, 130);
-            textName3.setSize(94,44);
+            textName3.setSize(94, 44);
+            game.addHighscoreText(textName3);
+
+            Button buttonName1 = new Button();
+            buttonName1.setPosition(12, 550);
+            buttonName1.setSize(181, 62);
+            buttonName1.setColour(255, 255, 255);
+            buttonName1.setText("Start");
+            buttonName1.Click += new System.EventHandler(game.btnStart_Click);
+            game.add(buttonName1);
+
+            gamePanel.add(createPlayer());
+            KeyDown += new KeyEventHandler(moveplayer);
+
+
+
 
 
             game.makeGame(this);
         }
-        private void movePlayer(object sender, KeyEventArgs key)
+        private void moveplayer(object sender, KeyEventArgs key)
         {
-            /*Player player = gamePanel.getPlayer();
+            Player player = gamePanel.getPlayer();
             if (key.KeyCode == Keys.D && game.checkPanelRight())
             {
-                
-            }
+                player.moveRight();    
+             }
             if (key.KeyCode == Keys.A && game.checkPanelLeft())
             {
-                
+                player.moveLeft(); 
             }
             if (key.KeyCode == Keys.W && game.checkPanelTop())
             {
+            player.moveUp();
                 
             }
             if (key.KeyCode == Keys.S && game.checkPanelBottom())
             {
-                
-            }*/
+            player.moveDown();    
+            }
+            player.setPosition(player.getPositionX(),player.getPositionY());
         }
         /*private void tmrGame_Tick(object sender, EventArgs e)
         {
         }*/
+
+        private Player createPlayer()
+        {
+            Player player = new Player();
+            player.setSize(50,50);
+            player.setSpeed(4);
+            player.setPosition(xPlayer,yPlayer);
+            return player;
+            
+        }
+
+
+
     }
 }
 
