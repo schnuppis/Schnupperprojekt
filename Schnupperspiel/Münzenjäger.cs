e3d3a9f54517e012309eac92b6040646bdd05a91
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 11:30:41 +0200

    Aufgabe 6

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -77,26 +77,41 @@ namespace Schnupperspiel
 
             game.makeGame(this);
 
+
+
+        }
+        private Player createPlayer()
+        {
+            Player player = new Player();
+            player.setSize(50, 50);
+            player.setSpeed(4);
+            player.setPosition(xPlayer, yPlayer);
+            createPlayer();
+            KeyDown += new KeyEventHandler(movePlayer);
+            return player;
+
         }
         private void movePlayer(object sender, KeyEventArgs key)
         {
-            /*Player player = gamePanel.getPlayer();
+            Player player = gamePanel.getPlayer();
             if (key.KeyCode == Keys.D && game.checkPanelRight())
             {
-                
+                player.moveRight();
             }
             if (key.KeyCode == Keys.A && game.checkPanelLeft())
             {
-                
+                player.moveLeft();
             }
             if (key.KeyCode == Keys.W && game.checkPanelTop())
             {
-                
+                player.moveUp();
             }
             if (key.KeyCode == Keys.S && game.checkPanelBottom())
             {
-                
-            }*/
+                player.moveDown();
+            }
+            player.getPositionX();
+            player.getPositionY();
         }
         /*private void tmrGame_Tick(object sender, EventArgs e)
         {
