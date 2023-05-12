16e8116515031902b43ae776c8202a309b537cd2
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 12:57:54 +0200

    Aufgabe 4

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -1,9 +1,10 @@
 using Schnupperspiel;
 using System;
+using System.Drawing.Text;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
- !! ALS STARTDATEI "Schnupperspiel.csproj - Debug|AnyCPU" auswählen !!
+ //!! ALS STARTDATEI "Schnupperspiel.csproj - Debug|AnyCPU" auswählen !!
 
 namespace Schnupperspiel
 {
@@ -46,20 +47,53 @@ namespace Schnupperspiel
             labelpoints.setPosition(820, 70);
             labelpoints.setSize(220, 55);
             labelpoints.setText("Points:");
-            game.add(labelName);
+            game.add(labelpoints);
            
             Label labelhighscore = new Label();
             labelhighscore.setPosition(820, 130);
             labelhighscore.setSize(220, 55);
             labelhighscore.setText("Highscore:");
-            game.add(labelName);
+            game.add(labelhighscore);
 
-            Text textName = new Text();
-            addTimeText(textName)
-            addPointsText(textName)
-            addHighscoreText(textName)
+            Text texttime = new Text();
+            texttime.setPosition(1050, 10);
+            texttime.setSize(94, 44);
+            game.addTimeText(texttime);
 
+            Text textpoints = new Text();
+            textpoints.setPosition(1050, 70);
+            textpoints.setSize(94, 44);
+            game.addPointsText(textpoints);
 
+            Text texthighscore = new Text();
+            texthighscore.setPosition(1050, 130);
+            texthighscore.setSize(94, 44);
+            game.addHighscoreText(texthighscore);
+
+            Button buttonName = new Button();
+            buttonName.setText("Start");
+            buttonName.setPosition(12, 550);
+            buttonName.setSize(181, 62);
+            buttonName.setColour(255, 255, 255);
+           
+            buttonName.Click += new System.EventHandler(game.btnStart_Click);
+           
+            Button ButtonStop = new Button();
+            ButtonStop.setPosition(423, 550);
+            ButtonStop.setSize(181, 62);
+            ButtonStop.setColour(255, 255, 255);
+            Button.setText("Stop");
+
+            buttonStop.Enabled = false;
+            buttonStop.Click += new System.EventHandler(game.btnStop_Click);
+            
+            private Player createPlayer()
+
+            
+            
+            
+            
+            
             game.makeGame(this);
         }
         private void movePlayer(object sender, KeyEventArgs key)
