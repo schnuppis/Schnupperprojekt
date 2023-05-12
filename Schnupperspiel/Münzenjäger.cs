c79a86e7861d36ba6c27b5ca7e50ff483c2e35e5
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 11:08:24 +0200

    4 fertig

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -21,14 +21,71 @@ namespace Schnupperspiel
         private void loadGame(object sender, EventArgs e)
         {
             
-            //game.setFormColour(200, 200, 200);
+            game.setFormColour(200, 200, 200);
 
-            //gamePanel = new Panel();
-            //gamePanel.setSize();
-            //gamePanel.setColour();
-           
+            gamePanel = new Panel();
+            gamePanel.setSize(800, 500);
+            gamePanel.setColour(0,0,0);
+            game.setPanel(gamePanel);
+
+           Label labelname = new Label();
+            labelname.setPosition(820, 480);
+            labelname.setSize(250, 55);
+            labelname.setText("Münzenjäger:");
+            game.add(labelname);
+
+           Label time = new Label();
+            time.setPosition(820, 10);
+            time.setSize(220, 55);
+            time.setText("Time:");
+            game.add(time);
+
+           Label points = new Label();
+            points.setPosition(820, 70);
+            points.setSize(220, 55);
+            points.setText("Points:");
+            game.add(points);
+
+           Label highscore = new Label();
+            highscore.setPosition(820, 130);
+            highscore.setSize(220, 55);
+            highscore.setText("Highscore:");
+            game.add(highscore);
+
+
+            Text TimeText = new Text();
+            TimeText.setPosition(1050, 10);
+            TimeText.setSize(94, 44);
+            game.addTimeText(TimeText);
+
+            Text PointsText = new Text();
+            PointsText.setPosition(1050, 70);
+            PointsText.setSize(94, 44);
+            game.addPointsText(PointsText);
+
+            Text HighscoreText = new Text();
+            HighscoreText.setPosition(1050, 130);
+            HighscoreText.setSize(94, 44);
+            game.addHighscoreText(HighscoreText);
 
             
+            Button startButton = new Button();
+            startButton.setPosition(12, 550);
+            startButton.setSize(181, 62);
+            startButton.setColour(255,255,255);
+            startButton.setText("Start");
+            startButton.Click += new System.EventHandler(game.btnStart_Click);
+            game.add(startButton);
+
+            Button stopButton = new Button();
+            stopButton.setPosition(423, 550);
+            stopButton.setSize(181, 62);
+            stopButton.setColour(255, 255, 255);
+            stopButton.setText("Stop");
+            stopButton.Enabled = false;
+            stopButton.Click += new System.EventHandler(game.btnStop_Click);
+            game.add(stopButton);
+
 
             game.makeGame(this);
         }
