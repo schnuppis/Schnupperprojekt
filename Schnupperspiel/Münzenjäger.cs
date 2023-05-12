5b1c250c04cc77f380da55fefbf2313fd94f49d0
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 11:09:54 +0200

    Aufgabe 1-3 gemacht

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -21,14 +21,44 @@ namespace Schnupperspiel
         private void loadGame(object sender, EventArgs e)
         {
             
-            //game.setFormColour(200, 200, 200);
+            game.setFormColour(200, 200, 200);
 
-            //gamePanel = new Panel();
-            //gamePanel.setSize();
-            //gamePanel.setColour();
-           
+            gamePanel = new Panel();
+            gamePanel.setSize(800,500);
+            gamePanel.setColour(0,0,0);
+            game.setPanel(gamePanel);
 
+            Label labelName = new Label();
+            labelName.setPosition(820, 480);
+            labelName.setSize(250, 55);
+            labelName.setText("Münzenjäger");
+            game.add(labelName);
+
+            Label labelTime = new Label();
+            labelTime.setSize(220, 55);
+            labelTime.setPosition(820, 10);
+            labelTime.setText("Time");
+            game.add(labelTime);
             
+
+            Label labelPoints = new Label();
+            labelPoints.setSize(220, 55);
+            labelPoints.setPosition(820, 70);
+            labelPoints.setText("Point");
+            game.add(labelPoints);
+
+
+            Label labelHighscore = new Label();
+            labelHighscore.setSize(220, 55);
+            labelHighscore.setPosition(820, 130);
+            labelHighscore.setText("Highscore");
+            game.add(labelHighscore);
+
+
+            Text textName = new Text();
+            game.addTimeText(textName);
+            game.addPointsText(textName);
+            game.addHighscoreText(textName);
 
             game.makeGame(this);
         }
@@ -55,5 +85,9 @@ namespace Schnupperspiel
         /*private void tmrGame_Tick(object sender, EventArgs e)
         {
         }*/
-    }
+       
+                 
+       }
+   
+        
 }
