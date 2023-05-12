b33eea9c7d6a03af085a1de893caf7d41c77379c
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 11:10:24 +0200

    Aufgabe 3 gemacht

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -20,15 +20,43 @@ namespace Schnupperspiel
         }
         private void loadGame(object sender, EventArgs e)
         {
-            
-            //game.setFormColour(200, 200, 200);
 
-            //gamePanel = new Panel();
-            //gamePanel.setSize();
-            //gamePanel.setColour();
-           
+            game.setFormColour(0, 0, 0);
 
-            
+
+            gamePanel = new Panel();
+            gamePanel.setSize(800, 500);
+            gamePanel.setColour(0, 0, 0);
+            game.setPanel(gamePanel);
+            Label labelName = new Label();
+            labelName.setPosition(820, 480);
+            labelName.setSize(250, 55);
+            labelName.setText("Münzenjäger");
+            game.add(labelName);
+            Label labeltime = new Label();
+            labeltime.setPosition(820, 10);
+            labeltime.setSize(220, 55);
+            labeltime.setText("Time");
+            game.add(labeltime);
+            Label labelpoints = new Label();
+            labelpoints.setPosition(820, 70);
+            labelpoints.setSize(220, 55);
+            labelpoints.setText("Points");
+            game.add(labelpoints);
+            Label labelhighscore = new Label();
+            labelhighscore.setPosition(820, 130);
+            labelhighscore.setSize(220, 55);
+            labelhighscore.setText("Highscore");
+            game.add(labelhighscore);
+            Text textName = new Text();
+            Text Timetext = new Text();
+            textName.setPosition(1050, 10);
+            textName.setSize(94, 44);
+            game.addTimeText(textName);
+
+
+
+
 
             game.makeGame(this);
         }
