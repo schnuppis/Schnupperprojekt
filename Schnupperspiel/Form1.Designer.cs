5ac81fa5b52273c5b0ff1452483056a28bc3527b
schnuppis <schnuppis.bit@gmail.com>
2023-05-12 11:09:01 +0200

    Aufgabe 4

--- Schnupperspiel/Form1.Designer.cs
+++ Schnupperspiel/Form1.Designer.cs
@@ -32,15 +32,16 @@ namespace Schnupperspiel
             this.SuspendLayout();
             // 
             // frmGame
-            //           
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2624, 1815);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.Name = "Münzenjäger";
+            this.ClientSize = new System.Drawing.Size(1079, 677);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmGame";
             this.Text = "Spielfeld";
             this.Load += new System.EventHandler(this.loadGame);
             this.ResumeLayout(false);
+
         }
 
         #endregion
--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -21,15 +21,73 @@ namespace Schnupperspiel
         private void loadGame(object sender, EventArgs e)
         {
             
-            //game.setFormColour(200, 200, 200);
+            game.setFormColour(200, 200, 200);
 
-            //gamePanel = new Panel();
-            //gamePanel.setSize();
-            //gamePanel.setColour();
-           
+            gamePanel = new Panel();
+            gamePanel.setSize(800,500);
+            gamePanel.setColour(0,0,0);
+            game.setPanel(gamePanel);
 
-            
+            Label Titellabel =new Label();
+            Titellabel.setPosition(820, 480);
+            Titellabel.setSize(250, 55);
+            Titellabel.setText("Münzenjäger");
 
+            Label TimeLabel = new Label();
+            TimeLabel.setPosition(820, 10);
+            TimeLabel.setSize(220, 55);
+            TimeLabel.setText("time:");
+
+            Label PointsLabel = new Label();
+            PointsLabel.setPosition(820, 70);
+            PointsLabel.setSize(220, 55);
+            PointsLabel.setText("Points:");
+
+            Label Highscore = new Label();
+            Highscore.setPosition(820, 130);
+            Highscore.setSize(220, 55);
+            Highscore.setText("Highscore:");
+
+            game.add(Highscore);
+            game.add(PointsLabel);
+            game.add(Titellabel);
+            game.add(TimeLabel);
+
+            Text TimeText = new Text();
+            TimeText.setPosition(1050, 10);
+            TimeText.setSize(94, 44);
+
+            Text PointsText = new Text();
+            PointsText.setPosition(1050, 70);
+            PointsText.setSize(94, 44);
+
+            Text HighscoreText = new Text();
+            HighscoreText.setPosition(1050, 130);
+            HighscoreText.setSize(94, 44);
+
+            game.add(HighscoreText);
+            game.add(PointsText);
+            game.add(TimeText);
+
+            Button startButton = new Button();
+            startButton.setPosition(12, 550);
+            startButton.setSize(181, 62);
+            startButton.setColour(255, 255, 255);
+            startButton.setText("Start");
+
+            startButton.Click += new System.EventHandler(game.btnStart_Click);
+
+            Button stopButton = new Button();
+            stopButton.setPosition(423, 550);
+            stopButton.setSize(181, 62);
+            stopButton.setColour(255, 255, 255);
+            stopButton.setText("Stop");
+
+            stopButton.Enabled = false;
+            stopButton.Click += new System.EventHandler(game.btnStop_Click);
+
+            game.add(startButton);
+            game.add(stopButton);
             game.makeGame(this);
         }
         private void movePlayer(object sender, KeyEventArgs key)
