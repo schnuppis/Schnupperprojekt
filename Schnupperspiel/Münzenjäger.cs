d61af2d5605850beeee784df7d63a2e75afdbd8a
schnuppis <schnuppis.bit@gmail.com>
2023-06-02 15:33:41 +0200

    Coins

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.Remoting.Channels;
 using System.Windows.Forms;
 
 // !! ALS STARTDATEI "Schnupperspiel.csproj - Debug|AnyCPU" auswählen !!
@@ -11,6 +12,9 @@ namespace Schnupperspiel
         private Random random = new Random();
         public static int xPlayer = 750;
         public static int yPlayer = 450;
+
+        public static int xcoin = 0;
+        public static int ycoin = 450;
 
         public Panel gamePanel;
 
@@ -27,130 +31,172 @@ namespace Schnupperspiel
             player.setPosition(xPlayer, yPlayer);
             return player;
         }
-
-        private void loadGame(object sender, EventArgs e)
+        private void createCoin()
+        {
+            Coin coin = new Coin();
+            coin.setSize(20, 20);
+            coin.addToList(game.getCoinList());
+            coin.setPosition(xcoin, ycoin, gamePanel);
+             
+        }
+        public void funkcoin(object sender, EventArgs e)
         {
             
-            game.setFormColour(200, 200, 200);
+            while (game.getCoinList().Count < 50) 
+            { 
+                xcoin= random.Next(20, gamePanel.getWidth() - 40);     
+                ycoin= random.Next(20, gamePanel.getWidth() - 40);
+                if (game.checkCoinPosition(xcoin, ycoin) == true)
+                {
+                    createCoin();
+                }
 
-            gamePanel = new Panel();
-            gamePanel.setSize(800,500);
-            gamePanel.setColour(0, 0, 0);
-            game.setPanel(gamePanel);
-            gamePanel.add(createPlayer());
-            
-            Label labelname = new Label();
-            labelname.setPosition(820, 480);
-            labelname.setSize(250, 55);
+                
+            }
 
-            labelname.setText("Münzenjäger:");
-
-            game.add(labelname);
-
-            Label Time = new Label();
-            Time.setPosition(820,10);
-            Time.setSize(220,55);
-
-            Time.setText("Time");
-
-            game.add(Time);
-
-            Label Points = new Label();
-            Points.setPosition(820, 70);
-            Points.setSize(220, 55);
-
-            Points.setText("Points");
-
-            game.add(Points);
-
-            Label Hightscore = new Label();
-            Hightscore.setPosition(820, 130);
-            Hightscore.setSize(220, 55);
-
-            Hightscore.setText("Hightscore");
-
-            game.add(Hightscore);
-
-            Text hightscore = new Text();
-            hightscore.setPosition(1050,130);
-            hightscore.setSize(94, 44);
-
-           
-
-            game.addHighscoreText(hightscore);
+        }
 
 
-            Text points = new Text();
-            points.setPosition(1050, 70);
-            points.setSize(94, 44);
+    private void loadGame(object sender, EventArgs e)
+    {
 
-           
+        game.setFormColour(200, 200, 200);
 
-            game.addPointsText(points);
+        gamePanel = new Panel();
+        gamePanel.setSize(800, 500);
+        gamePanel.setColour(0, 0, 0);
+        game.setPanel(gamePanel);
+        gamePanel.add(createPlayer());
 
-            Text time = new Text();
-            time.setPosition(1050, 10);
-            time.setSize(94, 44);
+        Label labelname = new Label();
+        labelname.setPosition(820, 480);
+        labelname.setSize(250, 55);
 
-            
+        labelname.setText("Münzenjäger:");
 
-            game.addTimeText(time);
+        game.add(labelname);
 
-            Button Start = new Button();
-            Start.setPosition(12, 550);
-            Start.setSize(181,62);
-            Start.setColour(0,255,0);
-            Start.setText("Start");
+        Label Time = new Label();
+        Time.setPosition(820, 10);
+        Time.setSize(220, 55);
 
-            Start.Click += new System.EventHandler(game.btnStart_Click);
-            game.add(Start);
+        Time.setText("Time");
 
-         
+        game.add(Time);
 
-            Button Stop = new Button();
-            Stop.setPosition(423, 550);
-            Stop.setSize(181, 62);
-            Stop.setColour(255, 0, 0);
-            Stop.setText("Stop");
+        Label Points = new Label();
+        Points.setPosition(820, 70);
+        Points.setSize(220, 55);
 
-            Stop.Click += new System.EventHandler(game.btnStop_Click);
-            game.add(Stop);
+        Points.setText("Points");
 
-            Stop.Enabled = false;
+        game.add(Points);
 
-            KeyDown += new KeyEventHandler(movePlayer);
+        Label Hightscore = new Label();
+        Hightscore.setPosition(820, 130);
+        Hightscore.setSize(220, 55);
 
-            Timer tmrGame = game.tmrGame;
-            tmrGame. Tick += new System.EventHandler(this.tmrGame_Tick);
-            KeyDown += new KeyEventHandler(movePlayer);
-            game.setTime(60);
-            game.setTimerGameInterval(100);
+        Hightscore.setText("Hightscore");
+
+        game.add(Hightscore);
+
+        Text hightscore = new Text();
+        hightscore.setPosition(1050, 130);
+        hightscore.setSize(94, 44);
+
+
+
+        game.addHighscoreText(hightscore);
+
+
+        Text points = new Text();
+        points.setPosition(1050, 70);
+        points.setSize(94, 44);
+        game.addPointsText(points);
+
+        Text time = new Text();
+        time.setPosition(1050, 10);
+        time.setSize(94, 44);
+
+
+
+        game.addTimeText(time);
+
+        Button Start = new Button();
+        Start.setPosition(12, 550);
+        Start.setSize(181, 62);
+        Start.setColour(0, 255, 0);
+        Start.setText("Start");
+
+        Start.Click += new System.EventHandler(game.btnStart_Click);
+        game.add(Start);
+
+
+
+        Button Stop = new Button();
+        Stop.setPosition(423, 550);
+        Stop.setSize(181, 62);
+        Stop.setColour(255, 0, 0);
+        Stop.setText("Stop");
+
+        Stop.Click += new System.EventHandler(game.btnStop_Click);
+        game.add(Stop);
+
+        Stop.Enabled = false;
+
+        KeyDown += new KeyEventHandler(movePlayer);
+
+        Timer tmrGame = game.tmrGame;
+        tmrGame.Tick += new System.EventHandler(this.tmrGame_Tick);
+        KeyDown += new KeyEventHandler(movePlayer);
+        game.setTime(120);
+        game.setTimerGameInterval(1000);
+
+            Timer tmrCoin = game.tmrGame;
+            tmrCoin.Tick += new System.EventHandler(this.funkcoin);
+
+
             game.makeGame(this);
 
-        }
-        private void movePlayer(object sender, KeyEventArgs key)
+    }
+    private void movePlayer(object sender, KeyEventArgs key)
+    {
+        Player player = gamePanel.getPlayer();
+        if (key.KeyCode == Keys.D && game.checkPanelRight())
         {
-            Player player = gamePanel.getPlayer();
-            if (key.KeyCode == Keys.D && game.checkPanelRight())
-            {
-                player.moveRight();
-            }
-            if (key.KeyCode == Keys.A && game.checkPanelLeft())
-            {
-                player.moveLeft();
-            }
-            if (key.KeyCode == Keys.W && game.checkPanelTop())
-            {
-                player.moveUp();
-            }
-            if (key.KeyCode == Keys.S && game.checkPanelBottom())
-            {
-                player.moveDown(); 
-            }
-            player.setPosition(player.getPositionX(), player.getPositionY());
+            player.moveRight();
         }
-        private void tmrGame_Tick(object sender, EventArgs e)
+        if (key.KeyCode == Keys.A && game.checkPanelLeft())
         {
+            player.moveLeft();
         }
+        if (key.KeyCode == Keys.W && game.checkPanelTop())
+        {
+            player.moveUp();
+        }
+        if (key.KeyCode == Keys.S && game.checkPanelBottom())
+        {
+            player.moveDown();
+        }
+        player.setPosition(player.getPositionX(), player.getPositionY());
+        game.LookForCoin(10);
+        game.setScore(game.getPoints());
+        }
+    private void tmrGame_Tick(object sender, EventArgs e)
+    {
+        if (game.getTime() == 0)
+        {
+            game.timeIsUp();
+            game.stopGame();
+               
+        }
+        }
+        private void tmrCoin_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+
     }
 }
+
