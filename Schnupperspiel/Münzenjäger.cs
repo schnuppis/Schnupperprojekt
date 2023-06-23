98d8b7840bad84f114855e52206cf422a5c77b2c
schnuppis <schnuppis.bit@gmail.com>
2023-06-23 10:44:57 +0200

    create

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -27,9 +27,49 @@ namespace Schnupperspiel
             gamePanel.setSize(800,500);
             gamePanel.setColour(0,0,0);
             game.setPanel(gamePanel);
-
+           
+            Label labelName = new Label();
+            labelName.setPosition(820, 480);
+            labelName.setSize(250, 55);
+            labelName.setText("Münzenjäger:");
+            game.add(labelName);
+           
+            Label timerName = new Label();
+            timerName.setPosition(820,10);
+            timerName.setSize(220,55);
+            timerName.setText("Time:");
+            game.add(timerName);
             
+            Label pointsName = new Label();
+            pointsName.setPosition(820,70);
+            pointsName.setSize(220,55);
+            pointsName.setText("Points:");
+            game.add(pointsName);
+            
+            Label highscoreName = new Label();
+            highscoreName.setPosition(820, 130);
+            highscoreName.setSize(220, 55);
+            highscoreName.setText("Highscore:");
+            game.add(highscoreName);
+            
+            Text textTime = new Text();
+            textTime.setPosition(1050, 10);
+            textTime.setSize(94, 44);
+            game.addTimeText(textTime);
+            
+            Text textPoints = new Text();
+            textPoints.setPosition(1050, 70);
+            textPoints.setSize(94, 44);
+            game.addPointsText(textPoints);
 
+            Text textHighscore = new Text();
+            textHighscore.setPosition(1050, 130);
+            textHighscore.setSize(94, 44);
+            game.addHighscoreText(textHighscore);
+            
+            
+            
+            
             game.makeGame(this);
         }
         private void movePlayer(object sender, KeyEventArgs key)
