c1caa3a706b0794a1bd2189d898137b5d803654a
schnuppis <schnuppis.bit@gmail.com>
2023-06-23 11:14:55 +0200

    Button

--- Schnupperspiel/Münzenjäger.cs
+++ Schnupperspiel/Münzenjäger.cs
@@ -27,6 +27,70 @@ namespace Schnupperspiel
             gamePanel.setSize(800, 500);
             gamePanel.setColour(0, 0, 0);
             game.setPanel(gamePanel);
+            
+            Label labelName = new Label();
+            labelName.setPosition(820, 480);
+            labelName.setSize(250, 55);
+            labelName.setText("Münzenjäger:");
+            game.add(labelName);
+            
+            Label timeName =new Label();
+            timeName.setPosition(820, 10);
+            timeName.setSize(220, 55);
+            timeName.setText("Time:");
+            game.add(timeName);
+
+            Label pointsName =new Label();
+            pointsName.setPosition(820, 70);
+            pointsName.setSize(220, 55);
+            pointsName.setText("Points:");
+            game.add(pointsName);
+
+            Label highscoreName =new Label();
+            highscoreName.setPosition(820, 130);
+            highscoreName.setSize(220, 55);
+            highscoreName.setText("Highscore:");
+            game.add(highscoreName);
+
+            
+            Text textTimer =new Text();
+            textTimer.setPosition(1050, 10);
+            textTimer.setSize(94, 44);
+            game.addTimeText(textTimer);
+
+            Text textPoints =new Text();
+            textPoints.setPosition(1050, 70);
+            textPoints.setSize(94, 44);
+            game.addPointsText(textPoints);
+
+            Text textHighscore = new Text();
+            textHighscore.setPosition(1050, 130);
+            textHighscore.setSize(94, 44);
+            game.addHighscoreText(textHighscore);
+
+            Button buttonStart =new Button();
+            buttonStart.setPosition(12, 550);
+            buttonStart.setSize(181, 62);
+            buttonStart.setColour(255, 255, 255);
+            buttonStart.setText("Start");
+            buttonStart.Click += new System.EventHandler(game.btnStart_Click);
+            game.add(buttonStart);
+
+            Button buttonEnd =new Button();
+            buttonEnd.setPosition(423, 550);
+            buttonEnd.setSize(181, 62);
+            buttonEnd.setColour(255, 255, 255);
+            buttonEnd.setText("End");
+            buttonEnd.Enabled= false;
+            buttonEnd.Click += new System.EventHandler(game.btnStop_Click);
+            game.add(buttonEnd);
+
+            
+            
+            
+            
+            
+            
            
 
             
